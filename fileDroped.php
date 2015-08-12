1ac9b0436269db44e585d07c09f7876ba84ebc19
Yann Orlarey <orlarey@grame.fr>
2015-08-12 19:48:09 +0200

    New file drag and drop approach

--- fileDroped.php
+++ fileDroped.php
@@ -5,12 +5,13 @@ require "php/functions.php";
 //session is started
 if (session_id()=="") session_start();
 
-$fn = (isset($_SERVER['HTTP_X_FILENAME']) ? $_SERVER['HTTP_X_FILENAME'] : false);
-if ($fn){
-  $tmpUploads = "/home/faust/www/onlinecompiler/tmp/".$_SESSION['id']."/uploads/";
-  file_put_contents($tmpUploads.$fn,file_get_contents('php://input'));
-  $_SESSION['fileDroped'] = 1;
-  $_SESSION['dropedFileName'] = $_SERVER['HTTP_X_FILENAME'];
+if(!empty($_FILES['file']['name'][0])){
+	foreach ($_FILES['file']['name'] as $position => $name) {
+    if(move_uploaded_file($_FILES['file']['tmp_name'][$position], '/home/faust/www/onlinecompiler/tmp/'.$_SESSION['id'].'/uploads/'.$name)){
+			$_SESSION['fileDroped'] = 1;
+			$_SESSION['dropedFileName'] = $name;
+		}
+	}
 }
 
 ?>
--- js/uploadDrag.js
+++ js/uploadDrag.js
@@ -1,48 +1,31 @@
 
-(function() {
-    function $id(id) {
-	return document.getElementById(id);
-    }
+(function  () {
 
-    function FileDragHover(e) {
-	e.stopPropagation();
-	e.preventDefault();
-	e.target.className = (e.type == "dragover" ? "hover" : "");
-    }
-    
-    function FileSelectHandler(e) {
-	FileDragHover(e);
-	var files = e.target.files || e.dataTransfer.files;
-	f = files[0];
-	UploadFile(f);
-	window.setTimeout('__goto__',250);
-	//document.location.replace("goto_codeFaust.php");
-	//__goto__
-    }
+	var dropzone = document.getElementById('filedrag');
 
-    function UploadFile(file) {
-	if (location.host.indexOf("sitepointstatic") >= 0) return
-	var xhr = new XMLHttpRequest();
-	if (xhr.upload && file.size <= $id("MAX_FILE_SIZE").value) {
-	    xhr.open("POST", $id("upload").action, true);
-	    xhr.setRequestHeader("X_FILENAME", file.name);
-	    xhr.send(file);
-	}
-    }
+	dropzone.ondragover = function() {
+		this.className = 'hover';
+		return false;
+	};
 
-    function Init() {
-	var filedrag = $id("filedrag");
-	var xhr = new XMLHttpRequest();
-	if (xhr.upload) {
-	    filedrag.addEventListener("dragover", FileDragHover, false);
-	    filedrag.addEventListener("dragleave", FileDragHover, false);
-	    filedrag.addEventListener("drop", FileSelectHandler, false);
-	    filedrag.style.display = "block";
-	}
-    }
+	dropzone.ondragleave = function(){
+		this.className = '';
+		return false;
+	};
 
-    if (window.File && window.FileList && window.FileReader) {
-	Init();
-    }
+	dropzone.ondrop = function(e){
+		e.preventDefault();
+		this.className = '';
+		var formData = new FormData(),
+		xhr = new XMLHttpRequest(),
+		x;
 
-})();
+		for (x = 0; x < e.dataTransfer.files.length; x = x + 1) {
+			formData.append('file[]',e.dataTransfer.files[x]);
+		}
+
+		xhr.open('post','fileDroped.php');
+		xhr.send(formData);
+	};
+
+}());

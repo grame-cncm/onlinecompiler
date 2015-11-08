d0408d3c3d268f001fda105cd8fb9a329c8d8cc4
Yann Orlarey <orlarey@grame.fr>
2015-11-08 18:05:18 +0100

    Check when a file is dropped if it's an URL. In this case uses the content of the URL as Faust code

--- fileDroped.php
+++ fileDroped.php
@@ -8,7 +8,21 @@ if (session_id()=="") session_start();
 
 if(!empty($_FILES['file']['name'][0])){
 	foreach ($_FILES['file']['name'] as $position => $name) {
-    	if(move_uploaded_file($_FILES['file']['tmp_name'][$position], $_SERVER['DOCUMENT_ROOT']."/onlinecompiler/tmp/".$_SESSION['id']."/uploads/$name")){
+		$filepath = $_SERVER['DOCUMENT_ROOT']."/onlinecompiler/tmp/".$_SESSION['id']."/uploads/$name";
+    	if(move_uploaded_file($_FILES['file']['tmp_name'][$position], $filepath)){
+			// Only small files are potential URLs
+			if (filesize($filepath) < 512) {
+				// check if file content is a valid URL
+				$content = file($filepath);
+				//file_put_contents('php://stderr', print_r($content, TRUE));
+				if (!filter_var($content[0], FILTER_VALIDATE_URL) === false) {
+					// Valid URL: we read the content of the URL and save it
+					$urlcontent = file($content[0]);
+					file_put_contents($filepath, $urlcontent);
+				} else {
+    				//file_put_contents('php://stderr', "Not an URL\n");
+				}
+			}
 			$_SESSION['fileDroped'] = 1;
 			$_SESSION['dropedFileName'] = $name;
 			//update();
